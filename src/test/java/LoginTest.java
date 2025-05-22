bfcc0fa27353255e7398e20017f9554b982bb36c
javier <j.droguett01@ufromail.cl>
2025-05-22 10:09:48 -0400

    Dependencias y Test Loginvalido

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+
+    private DatosLogin datosLogin;
+    private Login login;
+
+    @BeforeEach
+    public void setUp() {
+        datosLogin = new DatosLogin();
+        login = new Login();
+    }
+
+    @Test
+    public void testLoginValido() {
+        assertTrue(login.autenticar("nataly", "clave123", datosLogin));
+    }
+}
